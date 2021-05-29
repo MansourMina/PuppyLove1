e64e129c8fc2fde04b52ebc2076fec2831b3261d
Mina Mansour <mansourmina10@gmail.com>
2021-05-29 18:24:03 +0200

    finished

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,89 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-carousel :show-arrows="false" hide-delimiters cycle interval="3000">
+      <v-carousel-item
+        v-for="(item, i) in items"
+        :key="i"
+        :src="item.src"
+      ></v-carousel-item>
+    </v-carousel>
+    <span class="text-h4 d-flex justify-center my-3 green--text"
+      >Basset Hound Club of America
+    </span>
+
+    <div class="d-block text-center">
+      <v-btn class="ma-2" color="green darken-4 white--text">
+        MEMBER LOGIN
+      </v-btn>
+      <v-btn to="/member" class="ma-2" color="green darken-4 white--text">
+        BECOME A MEMBER
+      </v-btn>
+      <v-btn class="ma-2" color="green darken-4 white--text">
+        EVENTS
+      </v-btn>
+      <v-btn class="ma-2" color="green darken-4 white--text">
+        FIND A DOG
+      </v-btn>
+    </div>
+    <v-sheet class="mx-auto my-2" width="50%">
+      <div class="text-justify">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet.
+      </div>
+    </v-sheet>
+    <div class="d-flex flex-wrap justify-center">
+      <DogView v-for="d of dogs" :key="d.id" :dog="d" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import DogView from '@/components/DogView.vue';
 export default {
   name: 'Home',
+  data() {
+    return {
+      items: [
+        {
+          src:
+            'https://www.rover.com/blog/wp-content/uploads/2018/12/basset-hound-wallpaper-hd-2560x1440-214117.jpg',
+        },
+        {
+          src:
+            'https://www.welpen.de/cms/wp-content/uploads/2019/01/basset-hound-1.jpg',
+        },
+        {
+          src:
+            'https://loveandkissespetsitting.net/wp-content/uploads/2017/10/Basset-Hounds-3-of-them.jpg',
+        },
+      ],
+      dogs: [
+        {
+          id: 1,
+          name: 'Francis',
+          image:
+            'https://www.edogs.de/magazin/wp-content/uploads/basset-hound-auf-wiese.jpg',
+        },
+        {
+          id: 2,
+          name: 'Karambo',
+          image:
+            'https://www.zooroyal.at/magazin/wp-content/uploads/2018/02/basset-hound-760x570.jpg',
+        },
+        {
+          id: 3,
+          name: 'Mr. Phil',
+          image:
+            'https://mediafiles.mein-haustier.de/wp-content/uploads/2019/01/shutterstock_755551105-komprimiert-1270x847.jpg',
+        },
+      ],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    DogView,
+  },
+};
 </script>
---
+++ src/views/Member.vue
@@ -0,0 +1,118 @@
+<template>
+  <v-container>
+    <v-container>
+      <v-row justify="center">
+        <v-col md="6">
+          <div
+            class="text-center elevation-4 pa-12 headline green darken-4 white--text"
+          >
+            BECOME A MEMBER!
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-img
+      class="mt-5 mx-auto"
+      src="Club.jpg"
+      transition="scale-transition"
+      width="50%"
+    />
+    <v-form v-model="valid">
+      <v-row justify="center">
+        <v-col md="3">
+          <v-text-field
+            v-model="firstName"
+            counter="40"
+            hint="Please enter your last name"
+            label="First Name"
+            required
+            :rules="firstNameRules"
+          ></v-text-field
+        ></v-col>
+        <v-col md="3">
+          <v-text-field
+            v-model="lastName"
+            counter="10"
+            hint="Please enter your first name"
+            label="Last Name"
+            required
+            :rules="lastNameRules"
+          ></v-text-field
+        ></v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col md="3">
+          <v-text-field
+            prepend-inner-icon="mdi-at"
+            color="green darken-4"
+            hint="This field uses counter prop"
+            label="E-Mail"
+            :rules="emailRules"
+            required
+          ></v-text-field
+        ></v-col>
+        <v-col md="3">
+          <v-text-field
+            prepend-inner-icon="mdi-phone"
+            color="green darken-4"
+            hint="This field uses counter prop"
+            label="Phone"
+            :rules="phoneRules"
+            required
+          ></v-text-field
+        ></v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col md="6">
+          <v-checkbox
+            label="I agree to pay Robert Baumgartner 1000€ each month"
+            :rules="[(v) => !!v || 'You must agree to continue!']"
+            required
+          ></v-checkbox>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col md="6">
+          <div class="d-block text-center">
+            <v-btn
+              class="ma-2"
+              color="green darken-4 white--text"
+              :disabled="!valid"
+            >
+              Submit
+            </v-btn>
+          </div>
+        </v-col>
+      </v-row>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Member',
+  data: () => ({
+    valid: true,
+    firstName: '',
+    lastName: '',
+    firstNameRules: [
+      (v) => !!v || 'Name is required',
+      (v) => (v && v.length > 5) || 'Name must be at least 5 characters',
+      (v) => (v && v.length <= 40) || 'Name must be under 40 characters',
+    ],
+    lastNameRules: [
+      (v) => !!v || 'Name is required',
+      (v) => (v && v.length > 5) || 'Name must be at least 5 characters',
+      (v) => (v && v.length <= 10) || 'Name must be under 10 characters',
+    ],
+    emailRules: [
+      (v) => !!v || 'E-mail is required',
+      (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+    ],
+    phoneRules: [(v) => !!v || 'Phone Number is required'],
+  }),
+};
+</script>
+
+<style lang="scss" scoped></style>
